a9943b7ea7e10374444f9d1325433d493f88dd8a
Abdullah Amjad <shkabdullah268@gmail.com>
2025-11-09 00:50:05 -0800

    Adding Playervscomp Learning os.Exit() math/rand

--- stonePaperScissor/rps.go
+++ stonePaperScissor/rps.go
@@ -2,27 +2,26 @@ package main
 
 import "fmt"
 import "strings"
-
+import "math/rand"
+import "os"
 
 func main(){
-	// dict := map[string]int{
-	// 	"r" : 1,
-	// 	"p" : 2,
-	// 	"s" : 3, 
-	// }
+
 	var inp int 
+	for {
 	fmt.Println("---MY Rock Paper Scissor Game--- \n Press 0 For Exit \n 1. Player vs Player \n 2. Player vs Computer")
 	fmt.Scanln(&inp)
 	if inp == 1{
 		playervsplayer()
+	}else if inp == 2 {
+		palyervscomputer()
+	}else if inp == 0{
+		fmt.Println("Exiting...")
+		os.Exit(0)
+	}else{
+		fmt.Println("Invalid Try Again!")
 	}
-	// else if inp == 0 {
-	// 	...
-	// }
-	// else{
-	// 	...
-	// }
-
+	}
 }
 func playervsplayer(){
 			var p1 string
@@ -37,7 +36,7 @@ func playervsplayer(){
 					inp1 = 3
 			}else{
 					fmt.Println("Error")
-					// Exit the program
+					
 			}
 			var p2 string 
 			fmt.Println("Enter Player2's choice:")
@@ -50,11 +49,38 @@ func playervsplayer(){
 				inp2 = 3
 			}else{
 				fmt.Println("Error")
-				// Exit the program
+				
 			}
 			evaluate(inp1, inp2)
 		
 		}
+
+func palyervscomputer(){
+	moves := map[string]int{
+	"r":1,
+	"p":2,
+	"s":3,
+	}
+	var p1 string
+	var inp1 int  
+	fmt.Println("Enter the Player's input: ")
+	fmt.Scanln(&p1)
+	if strings.ToLower(p1) == "rock" || strings.ToLower(p1) == "r"{
+		inp1 = 1
+	}else if strings.ToLower(p1) == "paper" || strings.ToLower(p1) == "p"{
+		inp1 = 2
+	}else if strings.ToLower(p1) == "scissor" || strings.ToLower(p1) == "s"{
+		inp1 = 3
+	}else{
+		fmt.Println("Error")
+					
+	}
+	
+	fmt.Println("Making Player2's choice:")
+	no := rand.Intn(len(moves))
+	evaluate(inp1, no+1)
+
+}
 
 func evaluate (p1 int, p2 int ){
 
