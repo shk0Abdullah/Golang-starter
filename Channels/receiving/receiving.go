d06a485451b3f922e5f71627e63a567275f9ea7b
Abdullah Amjad <shkabdullah268@gmail.com>
2025-11-18 05:52:12 -0800

    goroutine synchronizer non buffer channels

--- Channels/receiving/receiving.go
+++ Channels/receiving/receiving.go
@@ -6,13 +6,24 @@ func sum(result chan int, no1 int, no2 int) {
 	result <- no1 + no2
 }
 
+func task(done chan bool) {
+	defer func() { done <- true }()
+	fmt.Println("Processing")
+}
+
 func main() {
 
-	result := make(chan int)
+	done := make(chan bool)
+	go task(done)
+	// We can do the same using channels instead of Waitgroup
+	// When you have multiple channels use Waitgroup
+	val := <-done //blocking
+	fmt.Println(val)
+	// result := make(chan int)
 
-	go sum(result, 4, 5)
-	// sending and receiving are blocking when we use goroutines excluding buffers
-	receive := <-result
-	fmt.Println(receive)
+	// go sum(result, 4, 5)
+	// // sending and receiving are blocking when we use goroutines excluding buffers
+	// receive := <-result
+	// fmt.Println(receive)
 
 }
